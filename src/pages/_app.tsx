0399744ab1ac7873c9610b2954df848ff77ff352
umer-maqsad <122520233+umer-maqsad@users.noreply.github.com>
2023-03-04 23:45:39 +0500

    [FEAT] Added basic configuration for Mantine UI

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,24 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.scss";
+import type { AppProps } from "next/app";
+import { MantineProvider } from "@mantine/core";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <MantineProvider
+      withGlobalStyles
+      withNormalizeCSS
+      withCSSVariables
+      theme={{
+        fontFamily: "Inter, sans-serif",
+        colors: {
+          black: ['#04152d', '#041226', '#020c1b'],
+          blue: ['#1c4b91', '#173d77'],
+          pink: ['#da2f68'],
+          orange: ['#f89e00'],
+        }
+      }}
+    >
+      <Component {...pageProps} />
+    </MantineProvider>
+  );
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,6 +1,12 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import { createGetInitialProps } from '@mantine/next';
+
+const getInitialProps = createGetInitialProps();
 
 export default function Document() {
+
+   const initialProps = getInitialProps;
+
   return (
     <Html lang="en">
       <Head />
